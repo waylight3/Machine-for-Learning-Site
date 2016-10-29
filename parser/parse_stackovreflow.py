7a826a17a7b0f22adaf7b53caa2a03667e76d663
waylight3 <waylight3@naver.com>
2016-10-30 00:13:08 +0900

    file read test

--- parser/parse_stackovreflow.py
+++ parser/parse_stackovreflow.py
@@ -1,7 +1,13 @@
 import urllib.request, json, sys, os
 from bs4 import BeautifulSoup
 
-url = 'http://stackoverflow.com/search?q=%s' % (keyword)
+with open('SearchList.txt', 'r') as fp:
+    keys = fp.read().split()
+
+print(keys)
+sys.exit()
+
+url = 'http://stackoverflow.com/search?q=%s' % (key)
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
 r = urllib.request.Request(url, headers=headers)
 p = urllib.request.urlopen(r)
